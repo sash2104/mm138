fe3be317ec7f49279cc6baa24804875555874347
sash <sash2104@gmail.com>
2022-07-15 11:49:31 +0000

    dice

---
+++ scripts/dice.py
@@ -0,0 +1,38 @@
+dice = [0, 1, 2, 3, 4, 5]
+to = [
+    [1, 5, 2, 0, 4, 3],
+    [3, 0, 2, 5, 4, 1],
+    [0, 4, 1, 2, 3, 5],
+    [0, 2, 3, 4, 1, 5],
+]
+
+vocab = dict()
+trans = [[0 for _ in range(4)] for _ in range(24)]
+
+def hash(cur):
+    return "".join(map(str,cur))
+
+def dfs(cur):
+    hcur = hash(cur)
+    cid = vocab[hcur]
+    for i in range(4):
+        nex = [0 for _ in range(6)]
+        for j in range(6):
+            nex[to[i][j]] = cur[j]
+        hnex = hash(nex)
+        skip = hnex in vocab
+        if not skip:
+            vocab[hnex] = len(vocab)
+        nid = vocab[hnex]
+        trans[cid][i] = nid
+        if not skip:
+            dfs(nex)
+
+
+vocab[hash(dice)] = len(vocab)
+dfs(dice)
+
+for k, v in vocab.items():
+    print(f'{{{",".join(list(k))}}},')
+for i in range(24):
+    print(f'{{{",".join(map(str,trans[i]))}}},')
